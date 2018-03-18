73dd44241e69209629fe5e28dcf920d7e792c081
lrakai <lmrakai@gmail.com>
2018-03-18 01:46:41 -0600

    finalize demo

--- src/3-resources/demo/main.tf
+++ src/3-resources/demo/main.tf
@@ -3,3 +3,15 @@ provider "google" {
   project = "cloud-academy-terraform"
   region  = "us-central1"
 }
+
+resource "google_compute_disk" "default" {
+  count = 3
+  name  = "test-disk-${count.index}"
+  type  = "pd-ssd"
+  image = "debian-8-jessie-v20170523"
+  zone  = "us-central1-a"
+
+  provisioner "local-exec" {
+    command = "echo disk ${count.index}: ${self.self_link} >> disk_urls.txt"
+  }
+}
